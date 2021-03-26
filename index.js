7fc7b15a4ad12b18e594907096ebd91c78d924e3
ryanspivey <rlspivey@go.olemiss.edu>
2021-03-26 11:14:03 -0500

    wrote the add function

--- index.js
+++ index.js
@@ -125,8 +125,8 @@ const zooAnimals = [
   /* 🦁🦁🦁 Step 2: Create several functions to callback with consume(); 🦁🦁🦁 */
  // 🦁🦁🦁 Use add to return the sum of two numbers 🦁🦁🦁
   
-function add(/*Your Code Here */){
-    /*Your Code Here*/
+function add(a, b){
+    return a + b;
   }
 
 // 🦁🦁🦁 Use multiply to return the product of two numbers 🦁🦁🦁
